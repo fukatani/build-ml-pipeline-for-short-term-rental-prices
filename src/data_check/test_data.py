5854290452c8984059e7fd4a7d3d0d688c07081e
Unknown <nannyakannya@gmail.com>
2024-08-21 20:28:01 +0900

    add comments.

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -64,8 +64,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 # Implement here test_row_count and test_price_range   #
 ########################################################
 def test_row_count(data: pd.DataFrame):
+    """
+    Test data has proper row size or not.
+    """
     assert 15000 < data.shape[0] < 1000000
 
 
 def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Test price outliers has excluded.
+    """
     assert data['price'].between(min_price, max_price).all()
